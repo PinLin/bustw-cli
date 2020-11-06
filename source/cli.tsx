e465cfe5773444695394dac20b19fd3397d7a2eb
Pin Lin <moneycat711@gmail.com>
2020-11-07 05:18:55 +0800

    Initial with create-ink-app

---
+++ source/cli.tsx
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+import React from 'react';
+import {render} from 'ink';
+import meow from 'meow';
+import App from './ui';
+
+const cli = meow(`
+	Usage
+	  $ bustw-cli
+
+	Options
+		--name  Your name
+
+	Examples
+	  $ bustw-cli --name=Jane
+	  Hello, Jane
+`, {
+	flags: {
+		name: {
+			type: 'string'
+		}
+	}
+});
+
+render(<App name={cli.flags.name}/>);
