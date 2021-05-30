641752af6ca895c48e3ab51172bc3d63c55e62fa
Pin Lin <pinlin945@gmail.com>
2021-05-30 23:30:42 +0800

    Create ShowStopsAndTime page

---
+++ source/page/show-stops-and-time.tsx
@@ -0,0 +1,66 @@
+import React, { FC, useState } from 'react';
+import { Text, useInput } from 'ink';
+import Spinner from 'ink-spinner';
+import { Tab, Tabs } from 'ink-tab';
+import useStdoutDimensions from 'ink-use-stdout-dimensions';
+import { getRepository } from 'typeorm';
+import { BusRoute } from '../entity/bus-route';
+import { BusStop } from '../entity/bus-stop';
+import { getCityChineseName } from '../util/city';
+
+export interface ShowStopsAndTimeProps {
+    city: string;
+    routeId: string;
+}
+
+export const ShowStopsAndTime: FC<ShowStopsAndTimeProps> = (props) => {
+    const [columns, rows] = useStdoutDimensions();
+    const [busRoute, setBusRoute] = useState(null as BusRoute);
+    const [subRouteTabIndex, setSubRouteTabIndex] = useState(0);
+    const [firstStopItemIndex, setFirstStopItemIndex] = useState(0);
+    useInput((input, key) => {
+        if (busRoute) {
+            const stops = JSON.parse(busRoute.subRoutes[subRouteTabIndex].stopsJson);
+            if (key.downArrow && firstStopItemIndex + (rows - 2) < stops.length) {
+                setFirstStopItemIndex(firstStopItemIndex + 1);
+            }
+            if (key.upArrow && firstStopItemIndex > 0) {
+                setFirstStopItemIndex(firstStopItemIndex - 1);
+            }
+        }
+    });
+
+    if (busRoute) {
+        return <>
+            <Text bold>{`[${getCityChineseName(busRoute.city)}] ${busRoute.nameZhTw}`}</Text>
+            <Tabs onChange={(name) => {
+                setSubRouteTabIndex(Number(name));
+                setFirstStopItemIndex(0);
+            }} children={busRoute.subRoutes.map((subRoute, index) => {
+                const stops = JSON.parse(subRoute.stopsJson) as BusStop[];
+                return <Tab name={index.toString()}>{`往${stops[stops.length - 1].nameZhTw}`}</Tab>
+            })} />
+            {
+                JSON.parse(busRoute.subRoutes[subRouteTabIndex].stopsJson).map((stop) => {
+                    return <Text>
+                        <Text color="gray">［ 載入中 ］</Text>
+                        <Text>{stop.nameZhTw}</Text>
+                    </Text>
+                }).slice(firstStopItemIndex, firstStopItemIndex + (rows - 2))
+            }
+        </>;
+    } else {
+        (async () => {
+            setBusRoute(await getRepository(BusRoute).findOne(props.routeId, {
+                relations: ['subRoutes'],
+            }));
+        })()
+
+        return <Text>
+            <Text color="green">
+                <Spinner type="dots" />
+            </Text>
+            {" Loading..."}
+        </Text>
+    }
+}
--- source/ui.tsx
+++ source/ui.tsx
@@ -2,20 +2,24 @@ import React, { FC, useState } from 'react';
 import { Text, Box } from 'ink';
 import useStdoutDimensions from 'ink-use-stdout-dimensions';
 import { getRepository } from 'typeorm';
+import { BusInfo } from './entity/bus-info';
 import { SelectCity } from './page/select-city';
 import { SearchRoute } from './page/search-route';
-import { BusInfo } from './entity/bus-info';
+import { ShowStopsAndTime } from './page/show-stops-and-time';
 
 enum AppState {
     None,
     SelectCity,
     SearchRoute,
+    ShowStopsAndTime,
 }
 
 const App: FC<{ name?: string }> = ({ name = 'Stranger' }) => {
     const [columns, rows] = useStdoutDimensions();
     const [appState, setAppState] = useState(AppState.None);
     const [availableCities, setAvailableCities] = useState([] as string[]);
+    const [selectedCity, setSelectedCity] = useState('');
+    const [selectedRouteId, setSelectedRouteId] = useState('');
 
     let page = <Text />
     if (appState == AppState.None) {
@@ -36,8 +40,13 @@ const App: FC<{ name?: string }> = ({ name = 'Stranger' }) => {
     }
     if (appState == AppState.SearchRoute) {
         page = <SearchRoute availableCities={availableCities} onSuccess={(city, routeId) => {
-            console.log(`${city} ${routeId}`)
+            setSelectedCity(city);
+            setSelectedRouteId(routeId);
+            setAppState(AppState.ShowStopsAndTime);
         }} />
+    }
+    if (appState == AppState.ShowStopsAndTime) {
+        page = <ShowStopsAndTime city={selectedCity} routeId={selectedRouteId} />
     }
 
     return <Box width={columns} height={rows} flexDirection="column">
