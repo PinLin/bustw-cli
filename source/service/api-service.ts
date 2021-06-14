c1dda1ecd024c3d527136f41bc3763ac8002dab6
Pin Lin <pinlin945@gmail.com>
2021-06-15 04:39:24 +0800

    Encode URI before calling API

--- source/service/api-service.ts
+++ source/service/api-service.ts
@@ -6,16 +6,16 @@ import { GetBusStopsDto } from '../dto/get-bus-stops';
 const host = 'http://localhost:3000'
 
 export async function getBusInfo(city: string) {
-  const response = await axios.get(`${host}/bus/city/${city}`);
+  const response = await axios.get(encodeURI(`${host}/bus/city/${city}`));
   return response.data as GetBusInfoDto;
 }
 
 export async function getBusRoutes(city: string) {
-  const response = await axios.get(`${host}/bus/city/${city}/routes`);
+  const response = await axios.get(encodeURI(`${host}/bus/city/${city}/routes`));
   return response.data as GetBusRoutesDto;
 }
 
 export async function getBusStopsByRouteName(city: string, routeName: string) {
-  const response = await axios.get(`${host}/bus/city/${city}/routes/${routeName}/stops`);
+  const response = await axios.get(encodeURI(`${host}/bus/city/${city}/routes/${routeName}/stops`));
   return response.data as GetBusStopsDto;
 }
